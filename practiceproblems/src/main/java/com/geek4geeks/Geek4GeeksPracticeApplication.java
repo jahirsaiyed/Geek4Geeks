2e1f01e986731ce88ea7cfaa6c5dd5148d3cb12b
jahiruddin.saiyed <jahiruddin.saiyed@emaratech.ae>
2021-03-13 18:44:44 +0400

    Project required files

---
+++ practiceproblems/src/main/java/com/geek4geeks/Geek4GeeksPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.geek4geeks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Geek4GeeksPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Geek4GeeksPracticeApplication.class, args);
+	}
+
+}
