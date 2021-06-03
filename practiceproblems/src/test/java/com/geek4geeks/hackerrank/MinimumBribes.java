be6729e3d49d3532bc6331d86e89a39d5b40ea5e
jahiruddin.saiyed <jahiruddin.saiyed@emaratech.ae>
2021-06-03 16:46:31 +0400

    Fixed hacker rank MinimumBribes

--- practiceproblems/src/test/java/com/geek4geeks/hackerrank/MinimumBribes.java
+++ practiceproblems/src/test/java/com/geek4geeks/hackerrank/MinimumBribes.java
@@ -8,24 +8,30 @@ import java.util.List;
 
 public class MinimumBribes {
 
-//    https://www.hackerrank.com/challenges/new-year-chaos/problem
+    //    https://www.hackerrank.com/challenges/new-year-chaos/problem
     public static int minimumBribes(List<Integer> q) {
         int totalBribes = 0;
         // Write your code here
         for (int i = 0; i < q.size(); i++) {
-             int bribe = q.get(i) - i - 1;
-             if (bribe > 2) {
-                 System.out.println("Too chaotic");
-                 return -1;
-             } else if(bribe > 0) {
-                 totalBribes += bribe;
-             }
+            int position = i + 1;
+            int bribe = q.get(i) - position;
+            if (bribe > 2) {
+                System.out.println("Too chaotic");
+                return -1;
+            }
+            while (position < q.size()) {
+                if(q.get(position) < q.get(i)) {
+                    totalBribes++;
+                }
+                position++;
+            }
         }
         return totalBribes;
     }
 
     @Test
     public void test() {
+        Assertions.assertEquals(-1, minimumBribes(Arrays.asList(5, 1, 2, 3, 4)));
         Assertions.assertEquals(3, minimumBribes(Arrays.asList(2, 1, 5, 3, 4)));
         Assertions.assertEquals(7, minimumBribes(Arrays.asList(1, 2, 5, 3, 7, 8, 6, 4)));
     }
